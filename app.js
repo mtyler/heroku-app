9047d2f9034ee5198f99eb7fb1c8aac5e90306ff
mtyler <mike_tyler@hotmail.com>
2014-02-08 18:41:46 -0500

    initial creation of files

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+//
+// Start http server and bind the socket.io service
+//
+var app = require("http").createServer(handler); // handler defined below
+//io = require("socket.io").listen(app);
+
+theport = process.env.PORT || 2000;
+app.listen(theport);
+console.log ("http server on port: " + theport);
+
+function handler (req, res) {
+  fs.readFile(__dirname + "/index.html",
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end("Error loading index.html");
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}
